2d756f25eab9ab5d0ce33a9a195f8a5a3e8b6630
John Lathrop <lathrop1009@gmail.com>
2025-01-10 17:54:35 -0500

    fixed the no toDo module

--- toDoList/urls.py
+++ toDoList/urls.py
@@ -18,7 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("toDo.urls")),
-    path("toDo/", include("toDo.urls")),
+
     path('admin/', admin.site.urls),
+    path("", include("toDo.urls")),
+    path("todo/", include("toDo.urls")),
 ]
