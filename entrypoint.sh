8fc2fba5dd7f1ee1c632dac27aa155f0feeb2c61
jjdubski <jjwaksmanski@gmail.com>
2025-01-10 23:45:16 -0600

    integrate Postgres, NGINX, gunicorn, docker and django together

---
+++ entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Attempting to enter postgres database..."
+
+    # Wait for the PostgreSQL database to be ready
+    while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
+        echo "Waiting for postgres..."
+    sleep 1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+# python manage.py flush --no-input
+# python manage.py migrate
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+exec "$@"
