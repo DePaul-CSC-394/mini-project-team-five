eb1558f25f9d6e1a867f3c8cf2679df8ed304e1a
John Lathrop <lathrop1009@gmail.com>
2025-01-12 22:55:01 -0600

    Adding Auth and Routing some pages

---
+++ users/models.py
@@ -0,0 +1,31 @@
+#https://www.youtube.com/watch?v=hN0xbn4sENw
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager, PermissionsMixin
+
+class userManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        return self.create_user(email, password, **extra_fields)
+        
+
+
+# Create your models here.
+class CustomUser(AbstractUser, PermissionsMixin):
+    email = models.EmailField(max_length=100, unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
+    objects= userManager()
