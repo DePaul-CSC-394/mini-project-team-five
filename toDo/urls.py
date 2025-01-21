cc9a7f6d719a884be259e8323fbbaad9f3f76a68
John Lathrop <lathrop1009@gmail.com>
2025-01-21 12:12:09 -0600

    Rename 'createToDo' URL to 'todos_new' for consistency across templates

--- toDo/urls.py
+++ toDo/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path("login/", views.login, name = "login"),
     path("dashboard/", views.dashboard, name = "dashboard"),
     path("teams/new", views.teamsNew, name = "teams_new"),
-    path("todos/new", views.todosNew, name = "createToDo"),
+    path("todos/new", views.todosNew, name = "todos_new"),
     path("todos/<int:id>/edit", views.todosEdit, name = "todos_edit"),
-    path('teams/<int:id>/', views.teams, name='teamList'),
+    path('teams/<int:id>/', views.teams, name='teams'),
     path("landing/", views.landing, name = "landing"),
     path("logout/", views.logoutView, name = "logout"),
 ]
