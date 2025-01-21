64c99cf9e643a18ce97530abe26f34d025f5e18b
John Lathrop <lathrop1009@gmail.com>
2025-01-20 18:10:11 -0600

    fixing connections to database

--- toDo/models.py
+++ toDo/models.py
@@ -10,7 +10,7 @@ from users.models import CustomUser
 class Team(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    members = models.ForeignKey(CustomUser, on_delete= models.CASCADE, default=None)
+    members = models.ManyToManyField(CustomUser, related_name='teams')
     
     def __str__(self):
         return self.name
