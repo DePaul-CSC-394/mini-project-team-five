a3a511dcc05a1edeae8d6da7a0cbeca39dba97a4
Jake <101855602+jjdubski@users.noreply.github.com>
2025-01-21 12:27:12 -0600

    Update 0002_initial.py

--- toDo/migrations/0002_initial.py
+++ toDo/migrations/0002_initial.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='team',
             name='members',
-            field=models.ManyToManyField(related_name='teamList', to=settings.AUTH_USER_MODEL),
+            field=models.ManyToManyField(related_name='teams', to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
             model_name='task',
