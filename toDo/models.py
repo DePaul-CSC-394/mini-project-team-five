1bf59844abba7f1e4fdc9b7b94b27f63c4dd6be6
John Lathrop <lathrop1009@gmail.com>
2025-01-26 20:49:39 -0500

    Refactor Team model and add TeamMember relationship; update login page forgot password link

--- toDo/models.py
+++ toDo/models.py
@@ -7,16 +7,16 @@ from users.models import CustomUser
 
 # Create your models here.
 
-class Team(models.Model):
-    name = models.CharField(max_length=200)
-    description = models.TextField(null=True, blank=True)
-    members = models.ManyToManyField(CustomUser, related_name='teams')
+# class Team(models.Model):
+#     name = models.CharField(max_length=200)
+#     description = models.TextField(null=True, blank=True)
+#     members = models.ManyToManyField(CustomUser, related_name='teams')
     
-    def __str__(self):
-        return self.name
+#     def __str__(self):
+#         return self.name
 
-    class Meta:
-        ordering = ['name']
+#     class Meta:
+#         ordering = ['name']
 
 class stopwatch(models.Model):
     startTime = models.DateTimeField(default=timezone.now, null=True, blank=True)
@@ -40,6 +40,21 @@ class stopwatch(models.Model):
             return self.endTime - self.startTime
         else:
             return None
+ 
+#New model for the team and member relationship
+#https://docs.djangoproject.com/en/5.1/topics/db/models/
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    members = models.ManyToManyField(CustomUser, through='TeamMember')
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+  
+ 
         
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
@@ -48,7 +63,7 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     dueDate= models.DateTimeField(null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
-    team = models.ForeignKey(Team, on_delete= models.CASCADE, default= None, null=True)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, null=True, blank=True)
     
     
     
@@ -61,3 +76,12 @@ class Task(models.Model):
     class Meta:
         ordering = ['complete']  # ordering within the database
         
+      
+        
+class TeamMember(models.Model):
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    member = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.member.email + ' is a member of ' + self.team.name
+    
